64ed9d0b5297ce8f73bfa5c5e733f553502ef0af
Omar-Rene <programacionomar@gmail.com>
2023-09-27 17:26:45 -0300

    Proyecto

--- pagina/index.js
+++ pagina/index.js
@@ -1,19 +1,21 @@
 fetch("/api/products")
-  .then((response) => response.json())
-  .then((data) => {
-    const productList = document.getElementById("product-list");
+    .then((response) => response.json())
+    .then((data) => {
+        const productList = document.getElementById("product-list");
 
-    data.forEach((product) => {
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `
-        <strong>${product.name}</strong><br>
-        Price: $${product.price}<br>
-        Description: ${product.description}<br>
-        <img src="${product.image_url}" alt="${product.name}" width="100"><br><br>
-      `;
-      productList.appendChild(listItem);
+        data.forEach((product) => {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `
+                <img src="${product.image_url}" alt="${product.name}">
+                <div class="product-info">
+                    <strong>${product.name}</strong><br>
+                    Price: $${product.price}<br>
+                    Description: ${product.description}<br>
+                </div>
+            `;
+            productList.appendChild(listItem);
+        });
+    })
+    .catch((error) => {
+        console.error("Error al obtener productos:", error);
     });
-  })
-  .catch((error) => {
-    console.error("Error al obtener productos:", error);
-  });
